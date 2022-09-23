276e5532266d49a85baf198dcdcbd84bd911f796
jungkeechu <jungkeechul@gmail.com>
2022-09-23 17:54:56 +0900

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ async def on_ready():
     )
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
+    print('I can understand only 'hi' and 'i miss you')
 
 
 # !!!!!`
